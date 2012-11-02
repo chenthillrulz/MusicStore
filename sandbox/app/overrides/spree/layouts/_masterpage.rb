e4212b325622ebe9ee7854626b8becc59e34f8c0
Jesse Naiman <jessenaiman@gmail.com>
2012-11-01 18:13:54 -0800

    minor changes, working towards getting the product page beautiful

--- sandbox/app/overrides/spree/layouts/_masterpage.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => 'spree/layouts/spree_application',
-								            :name => 'add_script_tag',
-								            :insert_after => "[data-hook='body']",
-								            :partial => "shared/script")
--- sandbox/app/overrides/spree/layouts/admin_tabs.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path  => "spree/layout/admin",
-                     :insert_after 	=> "[data-hook='admin_tabs']",
-                     :text          => "<p>Registration is the future!</p>",
-                     :name          => "registration_future")
